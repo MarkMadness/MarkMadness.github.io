f001db4b6ee6f24b4c78289e5c027b7faadb0fc9
Mark <1187productions@gmail.com>
2020-07-06 16:27:00 -0600

    Revised Portfolio for second round of job hunting

--- js/portfolio_singlepage.js
+++ js/portfolio_singlepage.js
@@ -6,32 +6,32 @@
 //     document.getElementById("Contact").style.display = "none";
 // }
 
-//SlideShow
-var slideIndex = 1;
-showSlides(slideIndex);
+////SlideShow
+//var slideIndex = 1;
+//showSlides(slideIndex);
 
-// Next/previous buttons
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+//// Next/previous buttons
+//function plusSlides(n) {
+//    showSlides(slideIndex += n);
+//}
 
-//Slides Content controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+////Slides Content controls
+//function currentSlide(n) {
+//    showSlides(slideIndex = n);
+//}
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("Slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i=0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i=0; i<dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+//function showSlides(n) {
+//    var i;
+//    var slides = document.getElementsByClassName("Slides");
+//    var dots = document.getElementsByClassName("dot");
+//    if (n > slides.length) {slideIndex = 1}
+//    if (n < 1) {slideIndex = slides.length}
+//    for (i=0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//    }
+//    for (i=0; i<dots.length; i++) {
+//        dots[i].className = dots[i].className.replace(" active", "");
+//    }
+//    slides[slideIndex-1].style.display = "block";
+//    dots[slideIndex-1].className += " active";
+//}
